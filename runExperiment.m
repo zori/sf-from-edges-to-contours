b70c4b1c13d2c880e8367a42ec506c50a6846c84
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2014-06-17 15:40:50 +0200

    Renamed variables.

--- runExperiment.m
+++ runExperiment.m
@@ -1,52 +1,52 @@
 function runExperiment()
 % SRF training and evaluation (using the VSB100 benchmark)
 
-close_parpool=false;
-to_log=false;
+closeParpool=false;
+logging=false;
 
 % example log files:
 % video_segm_evaluation/VSB100_40/test/recordings/2014-06-05_13-37-46/_recordings.txt - git version, runtimes
 % video_segm_evaluation/VSB100_40/test/recordings/2014-06-05_13-37-46/_recordings.mat - training, detection and benchmark options; benchmark output
 
-log_.dsFolder='VSB100_40';
-log_.model_name=[log_.dsFolder ''];
-log_.dsDir=fullfile('/BS/kostadinova/work/video_segm_evaluation', log_.dsFolder); % '/BS/kostadinova/work/BSR/BSDS500/data/';
-log_.recordings_dir=fullfile(log_.dsDir, 'test', 'recordings');
-log_.timestamp=datestr(clock,'yyyy-mm-dd_HH-MM-SS');
-log_.timestamp_dir=fullfile(log_.recordings_dir, log_.timestamp);
-log_.file=fullfile(log_.timestamp_dir, '_recordings.txt');
-log_.fid=1; % default is stdout
+LOG.dsFolder='VSB100_40';
+LOG.modelName=[LOG.dsFolder ''];
+LOG.dsDir=fullfile('/BS/kostadinova/work/video_segm_evaluation', LOG.dsFolder);
+LOG.recordingsDir=fullfile(LOG.dsDir, 'test', 'recordings');
+LOG.timestamp=datestr(clock,'yyyy-mm-dd_HH-MM-SS');
+LOG.timestampDir=fullfile(LOG.recordingsDir, LOG.timestamp);
+LOG.file=fullfile(LOG.timestampDir, '_recordings.txt');
+LOG.fid=1; % default is stdout
 
-if (to_log)
-  disp(log_.timestamp);
-  if (~exist(log_.recordings_dir, 'dir')), mkdir(log_.recordings_dir), end
-  mkdir(log_.timestamp_dir), log_.fid=fopen(log_.file, 'w');
+if (logging)
+  disp(LOG.timestamp);
+  if (~exist(LOG.recordingsDir, 'dir')), mkdir(LOG.recordingsDir), end
+  mkdir(LOG.timestampDir), LOG.fid=fopen(LOG.file, 'w');
 end
 cd(fileparts(mfilename('fullpath')));
-[status, git_commit_id]=system('git --no-pager log --format="%H" -n 1');
+[status, gitCommitId]=system('git --no-pager log --format="%H" -n 1');
 if (status), warning('no git repository in %s', pwd); end
-fprintf(log_.fid, 'Last git commit %s \n', git_commit_id);
+fprintf(LOG.fid, 'Last git commit %s \n', gitCommitId);
 
 %% Training
 
 % set opts for training (see edgesTrain.m)
-tr_opts=edgesTrain();                       % default options (good settings)
-tr_opts.modelDir='models/';                 % model will be in models/forest
-tr_opts.modelFnm=['model' log_.model_name]; % model name
-tr_opts.nPos=5e5;                           % decrease to speedup training
-tr_opts.nNeg=5e5;                           % decrease to speedup training
-tr_opts.useParfor=to_log;                   % parallelize if sufficient memory; true iff benchmarking
-tr_opts.dsDir=fullfile(log_.dsDir, 'train', filesep);
+trOpts=edgesTrain();                       % default options (good settings)
+trOpts.modelDir='models/';                 % model will be in models/forest
+trOpts.modelFnm=['model' LOG.modelName];   % model name
+trOpts.nPos=5e5;                           % decrease to speedup training
+trOpts.nNeg=5e5;                           % decrease to speedup training
+trOpts.useParfor=logging;                  % parallelize if sufficient memory; true iff benchmarking
+trOpts.dsDir=fullfile(LOG.dsDir, 'train', filesep);
 
 % train edge detector (~30m/15Gb per tree, proportional to nPos/nNeg)
-if (tr_opts.useParfor && isempty(gcp('nocreate')))
+if (trOpts.useParfor && isempty(gcp('nocreate')))
   pool=parpool(12);
   addAttachedFiles(pool,'/BS/kostadinova/work/video_segm/private/edgesDetectMex.mexw64');
 end
 
-timeEdgesTrain=tic;
-model=edgesTrain(tr_opts); % will load model if already trained
-training_time=toc(timeEdgesTrain);
+timerTr=tic;
+model=edgesTrain(trOpts); % will load model if already trained
+trainingTime=toc(timerTr);
 
 %% Detection
 
@@ -57,22 +57,22 @@ model.opts.nThreads=4;            % max number threads for evaluation; used in e
 model.opts.nms=false;             % set to true to enable nms (fairly slow)
 
 % run edge/segment detector
-det_opts={
-  'imgDir', fullfile(log_.dsDir, 'test/Images/'),...
-  'gtDir',  fullfile(log_.dsDir, 'test/Groundtruth/'),...
-  'resDir', fullfile(log_.dsDir, 'test/Ucm2/')};
+detOpts={
+  'imDir',  fullfile(LOG.dsDir, 'test/Images/'),...
+  'gtDir',  fullfile(LOG.dsDir, 'test/Groundtruth/'),...
+  'resDir', fullfile(LOG.dsDir, 'test/Ucm2/')};
 
-timeSegmDetect=tic;
-segmDetect(model, det_opts);
-detection_time=toc(timeSegmDetect);
+timerDet=tic;
+segmDetect(model, detOpts);
+detectionTime=toc(timerDet);
 
 %% Benchmark
 
-bm_opts.path=[log_.dsDir];         % path to bm_opts.dir
-bm_opts.dir='test';                % contains the directories `Images', `Groundtruth' and `Ucm2' (computed results of the algorithm of Dollar)
-bm_opts.nthresh=51;                % number of hierarchical levels to include
-bm_opts.superposeGraph=false;      % true - new curves are added to the same graph; false - a new graph is initialized
-bm_opts.testTempConsistency=true;  % false for images
+bmOpts.path=[LOG.dsDir];                      % path to bmOpts.dir
+bmOpts.dir='test';                            % contains the directories `Images', `Groundtruth' and `Ucm2' (computed results of the algorithm of Dollar)
+bmOpts.nthresh=51;                            % number of hierarchical levels to include
+bmOpts.superposeGraph=false;                  % true - new curves are added to the same graph; false - a new graph is initialized
+bmOpts.testTempConsistency=true;  % false for images
 % possible benchmark metrics
 metrics={
   'bdry',...       % BPR - Boundary Precision-Recall
@@ -82,26 +82,26 @@ metrics={
   'vi',...         % VI  - Variation of Information
   'lengthsncl',... % length statistics and number of clusters
   'all'};          % computes all available
-bm_opts.metric=metrics{end};
-bm_opts.outDir=fullfile('recordings', log_.timestamp);
+bmOpts.metric=metrics{end};
+bmOpts.outDir=fullfile('recordings', LOG.timestamp);
 
-timeBenchmark=tic;
+timerBm=tic;
 % Computerpimvid computes the Precision-Recall curves
-output=Computerpimvid(bm_opts.path, bm_opts.nthresh, bm_opts.dir,...
-  false, 0, 'r', bm_opts.superposeGraph, bm_opts.testTempConsistency,...
-  bm_opts.metric, [], bm_opts.outDir); %#ok<NASGU>
-benchmark_time=toc(timeBenchmark);
+output=Computerpimvid(bmOpts.path, bmOpts.nthresh, bmOpts.dir,...
+  false, 0, 'r', bmOpts.superposeGraph, bmOpts.testTempConsistency,...
+  bmOpts.metric, [], bmOpts.outDir); %#ok<NASGU>
+benchmarkTime=toc(timerBm);
 
-fprintf(log_.fid, 'Training %s \nDetection %s \nBenchmark %s\n\n',...
-  seconds2human(training_time),...
-  seconds2human(detection_time),...
-  seconds2human(benchmark_time));
+fprintf(LOG.fid, 'Training %s \nDetection %s \nBenchmark %s\n\n',...
+  seconds2human(trainingTime),...
+  seconds2human(detectionTime),...
+  seconds2human(benchmarkTime));
 
-if (close_parpool && ~isempty(gcp('nocreate'))), delete(gcp('nocreate')); end
+if (closeParpool && ~isempty(gcp('nocreate'))), delete(gcp('nocreate')); end
 
-if (to_log)
-  fclose(log_.fid);
-  save(fullfile(log_.timestamp_dir, '_recordings'),...
-    'tr_opts', 'det_opts', 'bm_opts', 'output');
+if (logging)
+  fclose(LOG.fid);
+  save(fullfile(LOG.timestampDir, '_recordings'),...
+    'trOpts', 'detOpts', 'bmOpts', 'output');
 end
 end
