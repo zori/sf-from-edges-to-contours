bb9db0f748c17ad625032f6c6264b4bc21b67d4c
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2014-07-06 17:55:19 +0200

    Moved relevant variables outside of the loop that asks for user input. Fixing user input so as to allow to crop an image patch centered at the input (bounce back to the inside of the image, not the boundary band).

--- patchesDemo.m
+++ patchesDemo.m
@@ -6,33 +6,35 @@ imFile='/BS/kostadinova/work/video_segm_evaluation/BSDS500/detect/Images/101085.
 % gtFile='/BS/kostadinova/work/video_segm_evaluation/BSDS500/detect/Groundtruth/101085.mat';
 I=imread(imFile);
 opts=model.opts;
+ri=opts.imWidth/2; % patch radius 16
+rg=opts.gtWidth/2; % patch radius 8
+nTreeNodes=length(model.fids);
+nTreesEval=opts.nTreesEval;
+% pad image, making divisible by 4
+szOrig=size(I); p=[ri ri ri ri];
+p([2 4])=p([2 4])+mod(4-mod(szOrig(1:2)+2*ri,4),4);
+IPadded=imPad(I,p,'symmetric');
+% compute feature channels
+[chnsReg,chnsSim]=edgesChns(IPadded,model.opts);
+% apply forest to image
+[Es,ind]=fooMex(model,chnsReg,chnsSim); % mex-file was private edgesDetectMex(...)
+% normalize and finalize edge maps
+% t=2*opts.stride^2/opts.gtWidth^2/opts.nTreesEval;
+% Es_=Es(1+rg:szOrig(1)+rg,1+rg:szOrig(2)+rg,:)*t; EsDetected=convTri(Es_,1);
+EsDetected=Es(1+rg:szOrig(1)+rg,1+rg:szOrig(2)+rg,:);
 
 while (true)
   clear functions; % clear the persistent vars in getFigPos
-  % get user input and crop patch TODO crop patch from Ipadded so as not to
-  % exceed matrix dimensions
+  % get user input and crop patch
   initFig(1); imagesc(I); axis('image'); title('Choose a patch to crop');
   [x,y]=ginput;
   % if no patch or more than one patch is selected, stop the interactive demo
   if (length(x)~= 1), close all; return; end
+  [x,y]=fixInput(x,y,ri,szOrig(1:2));
   x_ind=uint32(floor(x/2)); y_ind=uint32(floor(y/2));
   x=uint32(floor(x)); y=uint32(floor(y));
   initFig(); r0=opts.imWidth/2; % patch radius r=16
-  imagesc(cropPatch(I,x,y,r0)); axis('image'); title('Cropped image patch'); % TODO ... or, alternatively, "bounce" (move) patch within matrix bounds
-  
-  Im=I;
-  % pad image, making divisible by 4
-  szOrig=size(Im); r=opts.imWidth/2; p=[r r r r]; % r=16
-  p([2 4])=p([2 4])+mod(4-mod(szOrig(1:2)+2*r,4),4);
-  IPadded=imPad(Im,p,'symmetric');
-  % compute feature channels
-  [chnsReg,chnsSim]=edgesChns(IPadded,model.opts);
-  
-  % apply forest to image
-  [Es,ind]=fooMex(model,chnsReg,chnsSim); % mex-file was private edgesDetectMex(...)
-  % normalize and finalize edge maps
-  t=2*opts.stride^2/opts.gtWidth^2/opts.nTreesEval; r=opts.gtWidth/2; % r=8
-  Es_=Es(1+r:szOrig(1)+r,1+r:szOrig(2)+r,:)*t; EsDetected=convTri(Es_,1);
+  imagesc(cropPatch(I,x,y,r0)); axis('image'); title('Cropped image patch');
   
   % patch detected with the decision forest; result based on 16x16x4/4 trees
   % that vote for each pixel
@@ -41,13 +43,10 @@ while (true)
   ws=label2rgb(watershed(EsDetected),'jet',[.5 .5 .5]);
   initFig(); imagesc(cropPatch(ws,x,y,r0)); axis('image'); title('Superpixels patch');
   h=initFig(); im(zeros(r0,r0)); title('Placeholder intermediate decision patch');
-  
-  nTreeNodes=length(model.fids);
-  nTreesEval=opts.nTreesEval;
-  ids=double(ind(y_ind,x_ind,:)); % indices come from cpp and are 0-based; for tree_set=2 ??
+
+  ids=double(ind(y_ind,x_ind,:)); % indices come from cpp and are 0-based
   treeIds=uint32(floor(ids./nTreeNodes)+1);
   leafIds=uint32(mod(ids,nTreeNodes)+1);
-  
   for k=1:nTreesEval
     treeId=treeIds(:,:,k); leafId=leafIds(:,:,k);
     assert(~model.child(leafId,treeId)); % TODO add this to assertion (when also saving patches in forest) && ~isempty(model.patches{leafId,treeId}));
@@ -76,6 +75,13 @@ function patch = cropPatch(I,x,y,r)
 % crop a patch with radius r from an image I at location [x y]
 % output patch has dimensions [2r x 2r]
 patch=I(y-r+1:y+r,x-r+1:x+r,:);
+end
+
+% ----------------------------------------------------------------------
+function [x,y] = fixInput(x,y,r,szI)
+% return x y coordinates within the inside (not a band with width r) of the original image
+if y<r, y=r; else if y>szI(1)-r, y=szI(1)-r; end; end
+if x<r, x=r; else if x>szI(2)-r, x=szI(2)-r; end; end
 end
 
 function h = initFig(figureHandle)
