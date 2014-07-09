4a7d7e90fd10938e20c7f1b0074c93b73c512128
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2014-07-09 14:07:47 +0200

    create_finest_partition uses as an input a simple probability of boundary, not a (8D) matrix of oriented probabilities.

--- ucm/contours2ucm.m
+++ ucm/contours2ucm.m
@@ -1,4 +1,4 @@
-function [ucm] = contours2ucm(pb_oriented, fmt)
+function [ucm] = contours2ucm(pb, fmt)
 % Creates Ultrametric Contour Map from oriented contours
 %
 % syntax:
@@ -9,7 +9,7 @@ function [ucm] = contours2ucm(pb_oriented, fmt)
 %   the mean pb value on the boundary between regions as dissimilarity.
 %
 % arguments:
-%   pb_oriented: Oriented Probability of Boundary
+%   pb:          Probability of Boundary (not oriented)
 %   fmt:         Output format. 'imageSize' (default) or 'doubleSize'
 %
 % output:
@@ -24,7 +24,7 @@ if ~strcmp(fmt,'imageSize') && ~strcmp(fmt,'doubleSize'),
 end
 
 % create finest partition and transfer contour strength
-[ws_wt] = create_finest_partition(pb_oriented);
+[ws_wt] = create_finest_partition(pb);
 
 % prepare pb for ucm
 ws_wt2 = double(super_contour_4c(ws_wt));
@@ -45,56 +45,33 @@ if strcmp(fmt,'doubleSize'),
 else
     ucm = super_ucm(3:2:end, 3:2:end);
 end
+end
 
-%%
-
-function [ws_wt] = create_finest_partition(pb_oriented)
-
-pb = max(pb_oriented,[],3);
+% ----------------------------------------------------------------------
+function [ws_wt] = create_finest_partition(pb)
+% uses as an input a simple probability of boundary, not a (8D) matrix with
+% the probability of boundary for 8 orientations
 ws = watershed(pb);
 ws_bw = (ws == 0);
 
 contours = fit_contour(double(ws_bw));
-angles = zeros(numel(contours.edge_x_coords), 1);
-
-for e = 1 : numel(contours.edge_x_coords)
-    if contours.is_completion(e), continue; end
-    v1 = contours.vertices(contours.edges(e, 1), :);
-    v2 = contours.vertices(contours.edges(e, 2), :);
-
-    if v1(2) == v2(2),
-        ang = 90;
-    else
-        ang = atan((v1(1)-v2(1)) / (v1(2)-v2(2)));
-    end
-    angles(e) = ang*180/pi;
-end
-
-orient = zeros(numel(contours.edge_x_coords), 1);
-orient((angles<-78.75) | (angles>=78.75)) = 1;
-orient((angles<78.75) & (angles>=56.25)) = 2;
-orient((angles<56.25) & (angles>=33.75)) = 3;
-orient((angles<33.75) & (angles>=11.25)) = 4;
-orient((angles<11.25) & (angles>=-11.25)) =5;
-orient((angles<-11.25) & (angles>=-33.75)) = 6;
-orient((angles<-33.75) & (angles>=-56.25)) = 7;
-orient((angles<-56.25) & (angles>=-78.75)) = 8;
 
 ws_wt = zeros(size(ws_bw));
 for e = 1 : numel(contours.edge_x_coords)
     if contours.is_completion(e), continue; end
     for p = 1 : numel(contours.edge_x_coords{e}),
         ws_wt(contours.edge_x_coords{e}(p), contours.edge_y_coords{e}(p)) = ...
-            max(pb_oriented(contours.edge_x_coords{e}(p), contours.edge_y_coords{e}(p), orient(e)), ws_wt(contours.edge_x_coords{e}(p), contours.edge_y_coords{e}(p)));
+            max(pb(contours.edge_x_coords{e}(p), contours.edge_y_coords{e}(p)), ws_wt(contours.edge_x_coords{e}(p), contours.edge_y_coords{e}(p)));
     end
     v1=contours.vertices(contours.edges(e,1),:);
     v2=contours.vertices(contours.edges(e,2),:);
-    ws_wt(v1(1),v1(2))=max( pb_oriented(v1(1),v1(2), orient(e)),ws_wt(v1(1),v1(2)));
-    ws_wt(v2(1),v2(2))=max( pb_oriented(v2(1),v2(2), orient(e)),ws_wt(v2(1),v2(2)));
+    ws_wt(v1(1),v1(2))=max( pb(v1(1),v1(2)),ws_wt(v1(1),v1(2)));
+    ws_wt(v2(1),v2(2))=max( pb(v2(1),v2(2)),ws_wt(v2(1),v2(2)));
 end
 ws_wt=double(ws_wt);
-%%
+end
 
+% ----------------------------------------------------------------------
 function [pb2, V, H] = super_contour_4c(pb)
 
 V = min(pb(1:end-1,:), pb(2:end,:));
@@ -107,9 +84,9 @@ pb2(1:2:end, 2:2:end-2) = H;
 pb2(2:2:end-2, 1:2:end) = V;
 pb2(end,:) = pb2(end-1, :);
 pb2(:,end) = max(pb2(:,end), pb2(:,end-1));
+end
 
-%%
-
+% ----------------------------------------------------------------------
 function [ws_clean] = clean_watersheds(ws)
 % remove artifacts created by non-thin watersheds (2x2 blocks) that produce
 % isolated pixels in super_contour
@@ -172,8 +149,9 @@ for r = 1 : numel(R),
             ws_clean(xc-1, yc+1) = vec(4);
     end 
 end
+end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ----------------------------------------------------------------------
 function [pb_norm] = normalize_output(pb)
 % map ucm values to [0 1] with sigmoid 
 % learned on BSDS
@@ -185,9 +163,4 @@ pb_norm = 1 ./ (1 + (exp(-x'*beta)));
 pb_norm = (pb_norm - 0.0602) / (1 - 0.0602);
 pb_norm=min(1,max(0,pb_norm));
 pb_norm = reshape(pb_norm, [tx ty]);
-
-
-
-
-
-
+end
