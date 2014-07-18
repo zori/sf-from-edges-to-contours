391e9afb6534c0eafa577359d887758dfbdca79f
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2014-07-18 16:00:30 +0200

    Function now additionally returns the figure handles received from Plotsegmeval(). Better default parameter handling using Piotr toolbox's getPrmDflt().

--- Benchmark/ComputeRP.m
+++ Benchmark/ComputeRP.m
@@ -1,86 +1,87 @@
-function output=ComputeRP(filenames,nthresh,additionalmasname,requestdelconf,minimumimagenumber,curvecolor,plotsuperpose,considervideos,bmetrics,justavideo,outputdir)
-% ComputeRP(filenames,99,'tttttSegm',true,0,true,'r')
-% computes recall and precision for images or videos and plots the curves for
+function [output,fhs]=ComputeRP(varargin)
+% Computes recall and precision for images or videos and plots the curves for
 % the corresponding metrics
+%
+% OUTPUTS
+%  output         - structure summarising the evaluation results
+%  fhs            - array of figure handles to the plots
 %
 % Fabio Galasso
 % February 2014
 
-if (~exist('justavideo','var'))
-    justavideo=[];
-end
-if (~exist('outputdir','var'))
-    outputdir=[]; %The default directory in additionalmasname is defined in the function Benchmarkcreateoutimvid
-end
-if ( (~exist('curvecolor','var')) || (isempty(curvecolor)) )
-    curvecolor='r'; %rp curves color
-end
-if ( (~exist('plotsuperpose','var')) || (isempty(plotsuperpose)) )
-    plotsuperpose=false; %superpose rp curves
-end
-if ( (~exist('minimumimagenumber','var')) || (isempty(minimumimagenumber)) )
-    minimumimagenumber=0; %number of images to wait for starting computation (0 means no wait)
-end
-if ( (~exist('requestdelconf','var')) || (isempty(requestdelconf)) )
-    requestdelconf=true;
-end
-if ( (~exist('nthresh','var')) || (isempty(nthresh)) )
-    nthresh=5;
-end
-if ( (~exist('considervideos','var')) || (isempty(considervideos)) )
-    considervideos=true;
-end
-if ( (~exist('bmetrics','var')) || (isempty(bmetrics)) )
-    bmetrics={'bdry','regpr'}; %'bdry','3dbdry','regpr','sc','pri','vi','lengthsncl','all'
-end
-if ( (~exist('additionalmasname','var')) || (isempty(additionalmasname)) )
-    additionalmasname='tttttSegm';
-end
-if ( (~exist('filenames','var')) || (isempty(filenames)) )
-    filenames.benchmark=[pwd,filesep];
-end
-if (~isstruct(filenames))
-    tmp=filenames; clear filenames; filenames.benchmark=tmp; clear tmp;
-end
+% possible benchmark metrics
+metrics={
+  'bdry',...       % BPR - Boundary Precision-Recall
+  '3dbdry'...      % TODO is this not implemented
+  'regpr',...      % VPR - Volumetric Precision-Recall
+  'sc',...         % SC  - Segmentation Covering
+  'pri',...        % PRI - Probabilistic Rand Index
+  'vi',...         % VI  - Variation of Information
+  'lengthsncl',... % length statistics and number of clusters
+  'all'};          % computes all available
 
-% make sure it ends in a file separator
-filenames.benchmark = fullfile(filenames.benchmark, filesep);
+dfs={...
+  'path',pwd...                 % path to `dirR'
+  'dirR','tttttSegm',...        % directory (relative) that contains the directories `Images', `Groundtruth' and `Ucm2' (computed results)
+  'outDirR',[],...              % The default directory in dirR is defined in the function Benchmarkcreateoutimvid
+  'tempConsistency',true,...    % true for videos TODO do this automatically - we know whether a dataset is images or videos
+  'justavideo',[],...
+  'metrics',metrics{end},...    % could be a cell, e.g. {'bdry','regpr'}
+  'nthresh',5,...               % number of thresholds (hierarchical levels to include) for evaluating the ucm
+  'curveColor','r',...          % RP curves color
+  'superposePlot',false,...     % superpose RP curves; true - new curves are added to the same graph; false - a new graph is initialized
+  'confirmDel',false,...        % TODO is this interactive (if true) form useful
+  'minNumIms',0,...             % number of images to wait for starting computation (0 means no wait)
+  };
+
+opts=getPrmDflt(varargin,dfs,1);
+path_=opts.path;
+dirR=opts.dirR;
+outDirR=opts.outDirR;
+tempConsistency=opts.tempConsistency;
+justavideo=opts.justavideo;
+metrics=opts.metrics;
+nthresh=opts.nthresh;
+curveColor=opts.curveColor;
+superposePlot=opts.superposePlot;
+confirmDel=opts.confirmDel;
+minNumIms=opts.minNumIms;
+
+if (~isstruct(path_))
+    tmp=path_; clear path_; path_.benchmark=tmp; clear tmp;
+end
 
 %Assign input directory names and check existance of folders
 onlyassignnames=true;
-[sbenchmarkdir,imgDir,gtDir,inDir,isvalid] = Benchmarkcreatedirsimvid(filenames, additionalmasname, onlyassignnames); %#ok<ASGLU>
-%imgDir images (for name listing), gtDir ground truth, inDir ucm2, outDir output
+[~,imgDir,gtDir,inDir,isvalid] = Benchmarkcreatedirsimvid(path_, dirR, onlyassignnames);
+%imgDir images (for name listing), gtDir ground truth, inDir ucm2, outDirR output
 if (~isvalid)
     fprintf('Some Directories are not existing\n');
     return;
 end
 
-
-
 %Check existance of output directory and request confirmation of deletion
 onlyassignnames=true;
-[sbenchmarkdir,outDir,isvalid] = Benchmarkcreateoutimvid(filenames, additionalmasname, onlyassignnames, outputdir); %#ok<ASGLU>
+[dirA,outDirA,isvalid] = Benchmarkcreateoutimvid(path_, dirR, onlyassignnames, outDirR); %#ok<ASGLU>
 if ( isvalid )
-    if (requestdelconf)
+    if (confirmDel)
         theanswer = input('Remove previous output? [ 1 , 0 (default) ] ');
     else
         theanswer=0;
     end
     if ( (~isempty(theanswer)) && (theanswer==1) )
-        Removetheoutputimvid(filenames,additionalmasname,outputdir);
+        Removetheoutputimvid(path_,dirR,outDirR);
         isvalid=false;
     end
 end
 if (~isvalid)
-    [sbenchmarkdir,outDir,isvalid] = Benchmarkcreateoutimvid(filenames, additionalmasname, [], outputdir); %#ok<ASGLU>
+    [dirA,outDirA,isvalid] = Benchmarkcreateoutimvid(path_, dirR, [], outDirR); %#ok<ASGLU>
 end
 
-
-
-%Wait minimumimagenumber for processing
+%Wait minNumIms for processing
 iids=Listacrossfolders(imgDir,'jpg',Inf); % iids = dir(fullfile(imgDir,'*.jpg'));
-if (minimumimagenumber>0)
-    while(numel(iids)<minimumimagenumber)
+if (minNumIms>0)
+    while(numel(iids)<minNumIms)
         pause(10);
         iids=Listacrossfolders(imgDir,'jpg',Inf); % iids = dir(fullfile(imgDir,'*.jpg'));
     end
@@ -88,30 +89,19 @@ if (minimumimagenumber>0)
 end
 fprintf('%d images are in the folder (and first-level subfolders)\n',numel(iids));
 
-
-
 timeBmSegmEval = tic;
 if (isvalid)
-    Benchmarksegmeval(imgDir, gtDir, inDir, outDir, nthresh, [], [], bmetrics, considervideos,justavideo);
+    Benchmarksegmeval(imgDir, gtDir, inDir, outDirA, nthresh, [], [], metrics, tempConsistency,justavideo);
 end
 toc(timeBmSegmEval);
 
-
-
 timeBmEvalStats = tic;
 if (isvalid)
-    Benchmarkevalstats(imgDir, gtDir, inDir, outDir, nthresh, [], [], bmetrics, considervideos, justavideo);
+    Benchmarkevalstats(imgDir, gtDir, inDir, outDirA, nthresh, [], [], metrics, tempConsistency, justavideo);
 end
 toc(timeBmEvalStats);
 
+[output,fhs]=Plotsegmeval(outDirA,superposePlot,curveColor);
 
-
-output=Plotsegmeval(outDir,plotsuperpose,curvecolor);
-
-%     rmdir(sbenchmarkdir,'s')
-%     rmdir(outDir,'s')
-
-
-
-
-
+% rmdir(dirA,'s')
+% rmdir(outDirA,'s')
