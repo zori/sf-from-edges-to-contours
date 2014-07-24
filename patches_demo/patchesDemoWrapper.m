eb675b4a34e5d138f969ffb30660392c5c00e79b
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2014-07-24 16:05:57 +0200

    Patches demo loads the most recent BSDS model with the patches saved in the tree leaves.

--- patches_demo/patchesDemoWrapper.m
+++ patches_demo/patchesDemoWrapper.m
@@ -2,12 +2,12 @@
 % and decision trees
 
 if (~all(ismember({'model','T'},who)))
-  % load('/BS/kostadinova/work/video_segm/models/forest/modelBSDS500.mat'); % model
-  load('/BS/kostadinova/work/video_segm/models/forest/modelVSB100_40_patches.mat'); % model
+  load('/BS/kostadinova/work/video_segm/models/forest/modelBSDS500_patches.mat'); % model
+  % load('/BS/kostadinova/work/video_segm/models/forest/modelVSB100_40_patches.mat'); % model
   nTrees=8; T=cell(nTrees,1);
   for k=1:nTrees
-    % treeStr=['/BS/kostadinova/work/video_segm/models/tree/modelBSDS500_tree00' num2str(k) '.mat'];
-    treeStr=['/BS/kostadinova/work/video_segm/models/tree/modelVSB100_40_patches_tree00' num2str(k) '.mat'];
+    treeStr=['/BS/kostadinova/work/video_segm/models/tree/modelBSDS500_patches_tree00' num2str(k) '.mat'];
+    % treeStr=['/BS/kostadinova/work/video_segm/models/tree/modelVSB100_40_patches_tree00' num2str(k) '.mat'];
     T{k}=load(treeStr); T{k}=T{k}.tree;
   end
 end
