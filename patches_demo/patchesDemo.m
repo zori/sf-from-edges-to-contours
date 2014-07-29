62bf760cf010c9fbc82b428eeb850dd72ce022bb
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2014-07-29 14:16:54 +0200

    Added a function for computing the weights off superpixels patch and a given location.

--- patches_demo/patchesDemo.m
+++ patches_demo/patchesDemo.m
@@ -4,7 +4,8 @@ function patchesDemo(model,T)
 assert(~isempty(model) && ~isempty(T));
 
 % an image from BSDS500 validation subset
-imFile='/BS/kostadinova/work/video_segm_evaluation/BSDS500/detect/Images/101085.jpg';
+% imFile='/BS/kostadinova/work/video_segm_evaluation/BSDS500/detect/Images/101085.jpg';
+imFile='/BS/kostadinova/work/BSR/grouping/data/101087_small.jpg';
 % gtFile='/BS/kostadinova/work/video_segm_evaluation/BSDS500/detect/Groundtruth/101085.mat';
 I=imread(imFile);
 opts=model.opts;
@@ -25,11 +26,39 @@ t=2*opts.stride^2/opts.gtWidth^2/opts.nTreesEval;
 Es_=Es(1+rg:szOrig(1)+rg,1+rg:szOrig(2)+rg,:)*t; E=convTri(Es_,1);
 % E=Es(1+rg:szOrig(1)+rg,1+rg:szOrig(2)+rg,:);
 % Superpixelization (over-segmentation)
-ws=label2rgb(watershed(E),'jet',[1 1 1],'shuffle');
+ws=watershed(E);
+ws_bw=(ws==0);
+c=fit_contour(double(ws_bw));
+% remove empty fields
+c=rmfield(c,{'edge_equiv_ids','regions_v_left','regions_v_right','regions_e_left','regions_e_right','regions_c_left','regions_c_right'});
 % Ultrametric Contour Map
 ucm=contours2ucm(double(E)/255);
+computeWeightFun=@(x,y,spxPatch) computeWeights(x,y,spxPatch,model,T,I,opts,ri,rg,nTreeNodes,nTreesEval,szOrig,p,chnsReg,chnsSim,ind,E,ws,ucm);
 processLocationFun=@(x,y) processLocation(x,y,model,T,I,opts,ri,rg,nTreeNodes,nTreesEval,szOrig,p,chnsReg,chnsSim,ind,E,ws,ucm);
 
+%% processing SPX edges
+c.edge_weights=cell(size(c.edges,1),1);
+x=58; y=28;
+assert(isequal(1,c.is_e(x,y))); % it is an edge
+assert(isequal(0,c.is_v(x,y))); % it is not a vertex
+edgeInd=c.assign(x,y); % 48 the index of the edge % or 40
+vInds=c.edges(edgeInd,:); % [42 44] the two indices of the end vertices
+v1Coords=c.vertices(vInds(1),:); % [56    29]
+v2Coords=c.vertices(vInds(2),:); % [60    28]
+X=c.edge_x_coords(edgeInd); X=X{1}; % X'  [57    57    58    59]
+Y=c.edge_y_coords(edgeInd); Y=Y{1}; % Y'  [29    28    28    28]
+
+r=rg; % patch radius 8
+for k=1:length(X)
+  ex=X(k); ey=Y(k);
+  % TODO patch should be cropped from ws or from ucm?
+  spxPatch=cropPatch(ws,ex,ey,r); % superpixels patch
+  w=computeWeightFun(ex,ey,spxPatch);
+  c.edge_weights{edgeInd}=[w c.edge_weights{edgeInd}];
+end
+
+if 0
+%% interactive demo loop
 while (true)
   clear functions; % clear the persistent vars in getFigPos
   % get user input
@@ -43,7 +72,34 @@ while (true)
   hold on;
   processLocationFun(x,y);
 end % while(true)
+end
 end % patchesDemo
+
+% ----------------------------------------------------------------------
+function [w] = computeWeights(x,y,spxPatch,model,T,I,opts,ri,rg,nTreeNodes,nTreesEval,szOrig,p,chnsReg,chnsSim,ind,E,ws,ucm)
+% get 4 patches in leaves using .ind
+x1=ceil(((x+p(3))-opts.imWidth)/opts.stride)+rg; % rg<=x1<=w1, for w1 see edgesDetectMex.cpp
+y1=ceil(((y+p(1))-opts.imWidth)/opts.stride)+rg; % rg<=y1<=h1
+assert((x1==ceil(x/2)) && (y1==ceil(y/2)));
+ids=double(ind(y1,x1,:)); % indices come from cpp and are 0-based
+treeIds=uint32(floor(ids./nTreeNodes)+1);
+leafIds=uint32(mod(ids,nTreeNodes)+1);
+w=zeros(nTreesEval,1);
+for k=1:nTreesEval
+  treeId=treeIds(:,:,k); leafId=leafIds(:,:,k);
+  assert(~model.child(leafId,treeId)); % TODO add this to assertion (when also saving patches in forest) && ~isempty(model.patches{leafId,treeId}));
+  hs=T{treeId}.hs(:,:,leafId); % best segmentation
+  w(k)=patchDistance(spxPatch,hs);
+end
+end % computeWeights
+
+% ----------------------------------------------------------------------
+function d = patchDistance(patch1,patch2)
+% initFig(); im(patch1);
+% initFig(); im(patch2);
+% TODO compute distance
+d=0.75;
+end
 
 % ----------------------------------------------------------------------
 function processLocation(x,y,model,T,I,opts,ri,rg,nTreeNodes,nTreesEval,szOrig,p,chnsReg,chnsSim,ind,E,ws,ucm)
@@ -81,14 +137,15 @@ E4=Es4(1+rg:szOrig(1)+rg,1+rg:szOrig(2)+rg,:);
 % TODO why these apparent off-by-ones when cropping the patch; to "remedy" cropping 1px bigger radius; check rounding errors
 initFig(); im(cropPatch(E4,x,y,rg+1)); title('Intermediate decision patch (4 trees voted)');
 
-  % patch detected with the decision forest; result based on 16x16x4/4 trees
-  % that vote for each pixel
-  initFig(); im(cropPatch(E,x,y,rg)); title('SRF decision patch');
-  % Superpixelization (over-segmentation patch)
-  initFig(); imagesc(cropPatch(ws,x,y,rg)); axis('image'); title('Superpixels patch');
-  % Ultrametric Contour Map patch
-	h=initFig(); im(cropPatch(ucm,x,y,rg)); title('UCM patch');
+% patch detected with the decision forest; result based on 16x16x4/4 trees
+% that vote for each pixel
+initFig(); im(cropPatch(E,x,y,rg)); title('SRF decision patch');
+% Superpixelization (over-segmentation patch)
+initFig(); imagesc(label2rgb(cropPatch(ws,x,y,rg),'jet',[1 1 1],'shuffle')); axis('image'); title('Superpixels patch');
+% Ultrametric Contour Map patch
+h=initFig(); im(cropPatch(ucm,x,y,rg)); title('UCM patch');
 
+% TODO use a global var 'h' and a function cleanUpFigs to close old figs
 % remove all figures that were not created on this iteration
 figHandles=findobj('Type','figure');
 oldFigures=figHandles(figHandles>h); % h is the last handle used
@@ -120,6 +177,7 @@ function h = initFig(figureHandle)
 %  h            - figure handle
 %
 persistent cache figCnt;
+if isempty(figCnt), figCnt=1; end
 if nargin, figCnt=figureHandle; end
 if (~isempty(cache)), [scrSz,figSz,nFigs]=deal(cache{:}); else
   set(0,'Units','pixels');
