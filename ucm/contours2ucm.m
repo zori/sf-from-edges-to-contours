7e22d2e5dd48a4dc0b6ba3b90e79fa5368884411
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2014-08-27 21:04:44 +0200

    Restored the create_finest_partition_oriented() function, so now if the first argument of contours2ucm is an oriented pb, it uses the oriented (original procedure). Otherwise, a vanilla watershed will be applied.

--- ucm/contours2ucm.m
+++ ucm/contours2ucm.m
@@ -9,7 +9,7 @@ function [ucm] = contours2ucm(pb, fmt)
 %   the mean pb value on the boundary between regions as dissimilarity.
 %
 % arguments:
-%   pb:          Probability of Boundary (not oriented)
+%   pb:          Probability of Boundary (optionally oriented)
 %   fmt:         Output format. 'imageSize' (default) or 'doubleSize'
 %
 % output:
@@ -28,7 +28,12 @@ if ~strcmp(fmt,'imageSize') && ~strcmp(fmt,'doubleSize'),
 end
 
 % create finest partition and transfer contour strength
-[ws_wt] = create_finest_partition(pb);
+if ndims(pb) == 3
+  cfpFun=@(pb) create_finest_partition_oriented(pb);
+else
+  cfpFun=@(pb) create_finest_partition(pb);
+end
+[ws_wt] = cfpFun(pb);
 
 % prepare pb for ucm
 ws_wt2 = double(super_contour_4c(ws_wt));
@@ -74,6 +79,55 @@ for e = 1 : numel(contours.edge_x_coords)
 end
 ws_wt=double(ws_wt);
 end % create_finest_partition
+
+% ----------------------------------------------------------------------
+function [ws_wt] = create_finest_partition_oriented(pb_oriented)
+% 4.1 OWT
+pb = max(pb_oriented,[],3);
+ws = watershed(pb);
+ws_bw = (ws == 0);
+
+contours = fit_contour(double(ws_bw));
+angles = zeros(numel(contours.edge_x_coords), 1);
+
+for e = 1 : numel(contours.edge_x_coords)
+  if contours.is_completion(e), continue; end
+  v1 = contours.vertices(contours.edges(e, 1), :);
+  v2 = contours.vertices(contours.edges(e, 2), :);
+  
+  if v1(2) == v2(2),
+    ang = 90;
+  else
+    ang = atan((v1(1)-v2(1)) / (v1(2)-v2(2)));
+  end
+  angles(e) = ang*180/pi;
+end
+
+orient = zeros(numel(contours.edge_x_coords), 1);
+orient((angles<-78.75) | (angles>=78.75)) = 1;
+orient((angles<78.75) & (angles>=56.25)) = 2;
+orient((angles<56.25) & (angles>=33.75)) = 3;
+orient((angles<33.75) & (angles>=11.25)) = 4;
+orient((angles<11.25) & (angles>=-11.25)) =5;
+orient((angles<-11.25) & (angles>=-33.75)) = 6;
+orient((angles<-33.75) & (angles>=-56.25)) = 7;
+orient((angles<-56.25) & (angles>=-78.75)) = 8;
+
+% use the oriented contour detector output to assign each arc pixel a bdry strength
+ws_wt = zeros(size(ws_bw));
+for e = 1 : numel(contours.edge_x_coords)
+  if contours.is_completion(e), continue; end
+  for p = 1 : numel(contours.edge_x_coords{e}),
+    ws_wt(contours.edge_x_coords{e}(p), contours.edge_y_coords{e}(p)) = ...
+      max(pb_oriented(contours.edge_x_coords{e}(p), contours.edge_y_coords{e}(p), orient(e)), ws_wt(contours.edge_x_coords{e}(p), contours.edge_y_coords{e}(p)));
+  end
+  v1=contours.vertices(contours.edges(e,1),:);
+  v2=contours.vertices(contours.edges(e,2),:);
+  ws_wt(v1(1),v1(2))=max( pb_oriented(v1(1),v1(2), orient(e)),ws_wt(v1(1),v1(2)));
+  ws_wt(v2(1),v2(2))=max( pb_oriented(v2(1),v2(2), orient(e)),ws_wt(v2(1),v2(2)));
+end
+ws_wt=double(ws_wt);
+end % create_finest_partition_oriented
 
 % ----------------------------------------------------------------------
 function [pb2, V, H] = super_contour_4c(pb)
