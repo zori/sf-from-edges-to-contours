16df1814a555dc68f87e209dd21e60d748c05ed9
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2014-09-19 14:56:04 +0200

    Some comments regarding indexing images (matrics) vs. plotting order - x and y.

--- auxiliary/cropPatch.m
+++ auxiliary/cropPatch.m
@@ -5,9 +5,8 @@ function patch = cropPatch(I,x,y,r)
 % crop a patch with radius r from an image I at location [x y]
 % output patch has dimensions [2r x 2r]
 x=uint32(floor(x)); y=uint32(floor(y));
-% SZ=size(I); Y=SZ(1); X=SZ(2);
-% if Y<y+r ||X < x+r
-%   disp({x,y});
-% end
+% to see the location from where we're going to crop
+% figure; im(I);
+% hold on; plot(x,y,'rx','MarkerSize',20);
 patch=I(y-r+1:y+r,x-r+1:x+r,:);
 end
