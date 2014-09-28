bbfd876544e4ff5993c0bd38fbf58d2a8fd25ae5
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2014-09-28 11:25:37 +0200

    Spacing and comment.

--- ucm/voting/VPR.m
+++ ucm/voting/VPR.m
@@ -3,7 +3,7 @@
 % 8.3.0.532 (R2014a)
 % from Benchmark/Evaluatesegmregion.m
 function F = VPR(fst,snd)
-% returns F-value of the 
+% returns F-value of the VPR
 seg=fst;  % machine segmentation; output of algorithm; first patch
 groundTruth={struct('Segmentation',snd)}; % a cell with a structure with a field .Segmentation
 maxGtLabel=max(snd(:));  % maximum label in the second patch
@@ -35,14 +35,14 @@ end
 
 % Precision % Normalise precision (in case the biggest object is a zero the normalization is null)
 cntP=volumes.cntprect-normvolumes.cntprect; % Normalise: by definition seg only has 1 label and max is not necesary max(normoverlapseg,[],1)
-sumP=volumes.sumprect-normvolumes.cntprect; %Zeros labels are counted for the areas, -1 labels are not counted at all (regions of frames are ignored)
+sumP=volumes.sumprect-normvolumes.cntprect; % Zeros labels are counted for the areas, -1 labels are not counted at all (regions of frames are ignored)
 % Recall % Normalise
 cntR=volumes.cntrect-normvolumes.nofusedgts; % Normalise: the whole frame is used to take -1 into account and exclude gts with 0 areas
-sumR=volumes.sumrect-normvolumes.nofusedgts; %Zeros labels are counted for the areas, -1 labels are not counted at all (regions of frames are ignored)
+sumR=volumes.sumrect-normvolumes.nofusedgts; % Zeros labels are counted for the areas, -1 labels are not counted at all (regions of frames are ignored)
 
 % Benchmark/Collectevalaluatereg.m line 292
 % Precision recall and F measure for the considered segmentation
-R = cntR ./ (sumR + (sumR==0));
-P = cntP ./ (sumP + (sumP==0));
-F = fmeasure(R,P);
+R=cntR./(sumR+(sumR==0));
+P=cntP./(sumP+(sumP==0));
+F=fmeasure(R,P);
 end
