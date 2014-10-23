672a14f927fa06a67e18eb4181aa36f204024cb3
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2014-10-23 16:57:54 +0200

    Separated the code into 'precompute' and analysis part.

---
+++ ucm/voting/hard_negatives_mining_precompute.m
@@ -0,0 +1,68 @@
+% Zornitsa Kostadinova
+% Oct 2014
+% 8.3.0.532 (R2014a)
+function hard_negatives_mining_precompute()
+imFile='/BS/kostadinova/work/video_segm_evaluation/BSDS500/test/Images/100039.jpg';
+gtFile='/BS/kostadinova/work/video_segm_evaluation/BSDS500/test/Groundtruth/100039.mat';
+
+% bpr
+% Boundary PR global
+%    G-ODS: F( R 0.67, P 0.67 ) = 0.67   [th = 0.46]
+%    G-OIS: F( R 0.72, P 0.66 ) = 0.69
+%    Area_PR = 0.70
+% Volume PR global
+%    G-ODS: F( R 0.69, P 0.69 ) = 0.69   [th = 0.35]
+%
+% baseline
+% Boundary PR global
+%    G-ODS: F( R 0.70, P 0.69 ) = 0.69   [th = 0.27]
+%    G-OIS: F( R 0.73, P 0.73 ) = 0.73
+%    Area_PR = 0.75
+% Volume PR global
+%    G-ODS: F( R 0.71, P 0.73 ) = 0.72   [th = 0.25]
+%
+% oracle
+% Boundary PR global
+%    G-ODS: F( R 0.80, P 0.85 ) = 0.83   [th = 0.68]
+%    G-OIS: F( R 0.84, P 0.84 ) = 0.84
+%    Area_PR = 0.91
+% Volume PR global
+%    G-ODS: F( R 0.82, P 0.79 ) = 0.81   [th = 0.60]
+
+data=struct(...
+  'file',...
+  {'/BS/kostadinova/work/video_segm_evaluation/BSDS500/test/ucm2_precomputed/Ucm2_bpr_3/100039.mat',...
+  '/BS/kostadinova/work/video_segm_evaluation/BSDS500/test/ucm2_precomputed/Ucm2_sf_ucm/100039.mat',...
+  '/BS/kostadinova/work/video_segm_evaluation/BSDS500/test/ucm2_precomputed/Ucm2_oracle_bpr_3/100039.mat'},...
+  'threshold',{0.46, 0.27, 0.68}...
+  );
+dsz=numel(data);
+
+model=load('/BS/kostadinova/work/video_segm/models/forest/modelBSDS500.mat'); model=model.model;
+model.opts.multiscale=0;
+
+I=imread(imFile);
+gts=load_segmentations(gtFile);
+
+for k=1:dsz
+  uc=load(data(k).file); data(k).precomputed_ucm2=uc.ucm2(3:2:end, 3:2:end); % make the ucms imageSize
+  data(k).precomputed_seg=threshold_ucm2(data(k).precomputed_ucm2,data(k).threshold);
+end
+
+% vote indices - bpr3 and oracle
+vi=[1 3];
+% [ucm2,cfp_fcn]=ucm_weighted_bpr(I,model);
+patch_score_fcn=@(S,G) bpr(seg2bdry(S),seg2bdry(G),3);
+data(1).varargin={[]};
+data(3).varargin={[],gts};
+for k=vi
+  [cfp_fcn,E]=get_voting_fcn(I,model,patch_score_fcn,data(k).varargin{:});
+  [~,votes{k}]=cfp_fcn(E); % E == edgesDetect(I,model));
+  data(k).ucm2=contours2ucm(E,'imageSize',cfp_fcn);
+  data(k).seg=threshold_ucm2(data(k).ucm2,data(k).threshold);
+
+  % assert(all(data(k).ucm2(:)==data(k).ucm2_precomputed(:))); % no, because
+  % the bpr3 is an approximation, results will be slightly different
+end
+save('hard_negatives_mining');
+end
