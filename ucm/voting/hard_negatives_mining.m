8e9e6c1edf7188dc25e5b45bda65592a3b1eb4dd
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2014-10-29 16:13:41 +0100

    Don't write intermediate 'precomputed' results to a file, since because of the format (-v7.3) the output becomes too big and loading - as slow as computing from scratch. Instead, have two scripts that POLLUTE THE GLOBAL NAMESPACE and precompute functions, segmentations, votes, and UCMs for the hard_negative_mining tool.

--- ucm/voting/hard_negatives_mining.m
+++ ucm/voting/hard_negatives_mining.m
@@ -1,43 +1,27 @@
 % Zornitsa Kostadinova
 % Oct 2014
 % 8.3.0.532 (R2014a)
-load('hard_negatives');
-for k=vi, data(k).mean=cellfun(@mean,votes{k}); end
-
-for k=1:dsz, u{k}=unique(data(k).ucm2); end
-for k=1:dsz, l(k)=length(u{k}); end
-% "see" that values are roughly on the same scale
-% for k=1:sz, disp(u{k}(1:5)); end
-% for k=1:sz, disp(u{k}(end-5:end)); end
-
-d=abs(data(1).ucm2-data(3).ucm2); % diff with oracle
-% d=abs(ucms{1}-ucms{2}); % diff with baseline
-d=d .* ~(isnan(data(1).mean) | isnan(data(1).mean)); % TODO why isnan when we have voted there - fishy...
-sz=size(data(1).ucm2);
-r=16;
-[~,sort_ind]=sort(d(:),'descend'); % TODO instead, do top 5 unique
-maxIndex=sort_ind(1:555);  % linear index of the 5 largest values
-[ys,xs]=ind2sub(sz,maxIndex);
-inds=find(r<=ys & ys+r<=sz(1) & r <= xs & xs+r<=sz(2));
-
 % for k=1:length(gts), initFig(); im(gts{k}); end
 % for k=vi, initFig(); im(data(k).seg); end
 
 for ind=inds(1:10:100)'
   x=xs(ind); y=ys(ind);
-  initFig(1); im(data(3).ucm2); hold on; plot(x,y,'x','MarkerSize',20);
-  % for k=1:usz, initFig(); im(ucms{k}); hold on; plot(x(ind),y(ind),'x', 'MarkerSize',20); end
   for k=vi
-    cps{k}=cropPatch(data(k).ucm2,x,y,r);
-    initFig(); im(cps{k});
     disp(data(k).ucm2(y,x));
     disp(votes{k}{y,x}');
-    mcps{k}=cropPatch(data(k).mean,x,y,r);
-    initFig(); im(mcps{k}); hold on; plot(r,r,'x');
-    process_location_fcn{k}(x,y,votes{k}{y,x});
+    initFig(1); im(data(k).ucm2); hold on; plot(x,y,'x','MarkerSize',20);
+    ucm2crop=cropPatch(data(k).ucm2,x,y,r); initFig(); im(ucm2crop); hold on; plot(r,r,'x');
+    mean_crop=cropPatch(data(k).mean,x,y,r); initFig(); im(mean_crop); hold on; plot(r,r,'x');
+    if k == 1
+      process_location_fcn{k}(x,y,votes{k}{y,x});
+    else
+      % k == 3
+      % crop the gts
+      for kk=1:length(gts)
+        gt_crop=cropPatch(gts{kk},x,y,r);
+        initFig(); im(gt_crop); title(['ground truth ' num2str(kk), ' score ' num2str(votes{k}{y,x}(kk))]);
+      end
+    end
+    close all; % TIP: put a breakpoint here
   end
-  % initFig(); im(data(1).mean); hold on; plot(x,y,'x','MarkerSize',20)
-  % processLocation();
-end % TIP: put a breakpoint here
-
-close all;
+end
--- ucm/voting/hard_negatives_mining_precompute.m
+++ ucm/voting/hard_negatives_mining_precompute.m
@@ -1,14 +1,15 @@
 % Zornitsa Kostadinova
 % Oct 2014
 % 8.3.0.532 (R2014a)
-function hard_negatives_mining_precompute()
-model_name='modelBSDS500_patches'; %#ok<NASGU>
+model_name='modelBSDS500_patches';
 load_model_and_trees;
 model.opts.multiscale=0;
 
 imFile='/BS/kostadinova/work/video_segm_evaluation/BSDS500/test/Images/100039.jpg';
 gtFile='/BS/kostadinova/work/video_segm_evaluation/BSDS500/test/Groundtruth/100039.mat';
 
+% how the thresholds are chosen - based on the optimal ones when benchmarking
+%
 % bpr
 % Boundary PR global
 %    G-ODS: F( R 0.67, P 0.67 ) = 0.67   [th = 0.46]
@@ -42,8 +43,8 @@ data=struct(...
   );
 dsz=numel(data);
 
-I=imread(imFile);
-gts=load_segmentations(gtFile);
+I=imread(imFile); clear imFile;
+gts=load_segmentations(gtFile); clear gtFile;
 
 for k=1:dsz
   uc=load(data(k).file); data(k).precomputed_ucm2=uc.ucm2(3:2:end, 3:2:end); % make the ucms imageSize
@@ -52,7 +53,6 @@ end
 
 % vote indices - bpr3 and oracle
 vi=[1 3];
-% [ucm2,cfp_fcn]=ucm_weighted_bpr(I,model);
 patch_score_fcn=@(S,G) bpr(seg2bdry(S),seg2bdry(G),3);
 data(1).varargin={T};
 data(3).varargin={T,gts};
@@ -65,6 +65,22 @@ for k=vi
   % assert(all(data(k).ucm2(:)==data(k).ucm2_precomputed(:))); % no, because
   % the bpr3 is an approximation, results will be slightly different
 end
-% save('hard_negatives_small','data','votes','process_location_fcn','vi','dsz'); % no, because of 'data'
-save('hard_negatives','-v7.3','data','votes','process_location_fcn','vi','dsz');
-end
+for k=vi, data(k).mean=cellfun(@mean,votes{k}); end
+
+for k=1:dsz, u{k}=unique(data(k).ucm2); end
+for k=1:dsz, l(k)=length(u{k}); end
+% "see" that values are roughly on the same scale
+% for k=1:sz, disp(u{k}(1:5)); end
+% for k=1:sz, disp(u{k}(end-5:end)); end
+
+d=abs(data(1).ucm2-data(3).ucm2); % diff with oracle
+% TODO diff with baseline
+d=d .* ~(isnan(data(1).mean) | isnan(data(3).mean)); % TODO why isnan when we have voted there - fishy...
+sz=size(data(1).ucm2);
+r=16;
+[~,sort_ind]=sort(d(:),'descend'); % TODO instead, do top 5 unique
+maxIndex=sort_ind(1:555);  % linear index of the 5 largest values
+[ys,xs]=ind2sub(sz,maxIndex);
+inds=find(r<=ys & ys+r<=sz(1) & r<=xs & xs+r<=sz(2));
+
+clear imFile I E uc d sz sort_ind maxIndex;
