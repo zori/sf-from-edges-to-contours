644e9115ab070a620594eab4844c8c98ac63b4a2
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2014-11-04 20:03:12 +0100

    Added 'p' - padding to the interface of process_location_gt to allow to pad the images when investigating (inspecting, looking into) the oracle case.

--- ucm/voting/hard_negatives_mining.m
+++ ucm/voting/hard_negatives_mining.m
@@ -10,15 +10,16 @@ for ind=inds(1:10:100)'
     disp(data(k).ucm2(y,x));
     disp(votes{k}{y,x}');
     initFig(1); im(data(k).ucm2); hold on; plot(x,y,'x','MarkerSize',20);
-    ucm2crop=cropPatch(data(k).ucm2,x,y,r); initFig(); im(ucm2crop); hold on; plot(r,r,'x');
-    mean_crop=cropPatch(data(k).mean,x,y,r); initFig(); im(mean_crop); hold on; plot(r,r,'x');
+    ucm2crop=cropPatch(data(k).ucm2,x,y,r); pshow(ucm2crop);
+    mean_crop=cropPatch(data(k).mean,x,y,r); pshow(mean_crop);
     if k == 1
       % bpr3
       process_location_fcn{k}(x,y,votes{k}{y,x});
     else
       % k == 3
       % crop the gts
-      process_location_gt(x,y,votes{k}{y,x},gts,r);
+      p=[0 0 0 0]; % no padding, kind of hack, since we are only cropping away from the boundary to avoid the problem
+      process_location_gt(x,y,votes{k}{y,x},gts,p,r);
     end
     close all; % TIP: put a breakpoint here
   end
