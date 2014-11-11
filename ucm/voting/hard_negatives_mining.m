ab71e701ce0bed566f303fa72969cc0d7502fe9b
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2014-11-11 22:21:35 +0100

    Have the hard_negative_mining use line_VPR_normalised_ws as an experiment setup, since it is possible to reproduce the results (no randomisation, no approximations).

--- ucm/voting/hard_negatives_mining.m
+++ ucm/voting/hard_negatives_mining.m
@@ -8,13 +8,14 @@ for ind=inds(1:10:100)'
   x=xs(ind); y=ys(ind);
   for k=vi
     disp(data(k).ucm2(y,x));
-    disp(votes{k}{y,x}'); % this is [] in the first case; investigate why; don't rerun 'precompute' as the method is randomized!!!!
+    disp(data(k).votes{y,x}');
     initFig(1); im(data(k).ucm2); hold on; plot(x,y,'x','MarkerSize',20);
     show_patch(data(k).mean,x,y,r,'crop of the mean of votes');
-    show_patch(sf_wt{k},x,y,r,'crop of the finest partition');
+    show_patch(data(k).sf_wt,x,y,r,'crop of the finest partition');
     show_patch(data(k).ucm2,x,y,r,'crop of the ucm2');
     dbg=true; % will pause after displaying the patches
-    vote_fcn{k}(x,y,{c{k},c{k}.is_e(y,x),sz},dbg);
+    c=data(k).c;
+    data(k).vote_fcn(x,y,{c,c.is_e(y,x),sz},dbg);
     close all;
   end
 end
