888d4bb51529bc18b522ed8648fe3fa662d01916
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2014-11-11 21:18:29 +0100

    Have the demo up-to-date with recent changes. First step towards making it a rudimentary test.

--- ucm/voting/demo_ucm_weighted.m
+++ ucm/voting/demo_ucm_weighted.m
@@ -4,29 +4,28 @@
 model_name='modelBSDS500_patches';
 load_model_and_trees;
 fmt='doubleSize';
+dbg=true;
+% strings describing all possible types of vote to weigh the watershed
+votings={'bpr','greedy_merge','line_VPR_normalised_ws'}; % RSRI RI vpr_s vpr_gt
 
 %% 16x16 vertical line
 l=zeros(16,16); l(:,8)=1;
 L=repmat(l,1,1,3); clear l;
-L_ucm=ucm_weighted(L,model,'vpr',fmt,T);
-merge_L_ucm=ucm_weighted(L,model,'greedy_merge',fmt,T);
+L_ucm=ucm_weighted(L,model,'line_VPR_normalised_ws',fmt,true,T); % interactive, pause when computing
+
+merge_L_ucm=ucm_weighted(L,model,'greedy_merge',fmt,false,T); % don't pause, just compute
 
 %% bw diagonal
 bw=repmat(eye(260),1,1,3);
-
-merge_bw_ucm=ucm_weighted(bw,model,'greedy_merge',fmt,T);
-
-bpr_bw_ucm=ucm_weighted_bpr(bw,model,T);
 bw_gt=watershed(bw(:,:,1),4); bw_gt(bw_gt==0)=1;
-bpr_bw_ucm_oracle=ucm_weighted_bpr(bw,model,T,{bw_gt});
-
-rsri_bw_ucm=ucm_weighted(bw,model,'RSRI',fmt,T);
-
-vpr_s_bw_ucm=ucm_weighted(bw,model,'vpr_s',fmt,T);
+for k=1:length(votings)
+  bw_res{k}=ucm_weighted(bw,model,votings{k},fmt,dbg,T);
+  bw_oracle{k}=ucm_weighted(bw,model,votings{k},fmt,T,dbg,{bw_gt});
+end
 
 %% real image
 imFile='/BS/kostadinova/work/BSR/BSDS500/data/images/val/101085.jpg';
 gtFile='/BS/kostadinova/work/BSR/BSDS500/data/groundTruth/val/101085.mat';
 I=imread(imFile);
-ucm=ucm_weighted(I,model,'RI',fmt,T);
-ucm_oracle=ucm_weighted(I,model,'greedy_merge',fmt,T,load_segmentations(gtFile));
+% ucm=ucm_weighted(I,model,'RI',fmt,T);
+% ucm_oracle=ucm_weighted(I,model,'greedy_merge',fmt,T,load_segmentations(gtFile));
