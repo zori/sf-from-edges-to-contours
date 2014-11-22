5d124d8d265bcc90881f03ac27599484ef7bf1c4
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2014-11-22 13:54:06 +0100

    Script to analyse and rescale a directory containing ucm2's. Input processing and data analysis ready. TODO rescale and save.

---
+++ auxiliary/rescale_ucm2.m
@@ -0,0 +1,48 @@
+% Zornitsa Kostadinova
+% Nov 2014
+% 8.3.0.532 (R2014a)
+% function rescale_ucm2(indir_experiment_name)
+% input
+indir_experiment_name='ucm_bdry_sz_1_ul_1x1';
+path_to_dir='/BS/kostadinova/work/video_segm_evaluation/BSDS500/test';
+indir.r = ['Ucm2_' indir_experiment_name]; % relative directory name
+indir.a = fullfile(path_to_dir,indir.r); % absolute directory name
+ucm2s=dir(fullfile(indir.a,'*mat'));
+ucm2sz=[643 963];
+ucm2n=length(ucm2s);
+assert(ucm2n==200);
+
+% analysis
+data=repmat(struct('ucm2',zeros(ucm2sz)),ucm2n,1);
+for k=1:ucm2n
+  u=load(fullfile(indir.a,ucm2s(k).name)); u=u.ucm2;
+  data(k).ucm2=u;
+  data(k).nonzeros=u(u~=0);
+  data(k).num_nonzeros=length(data(k).nonzeros);
+  un=unique(data(k).nonzeros);
+  data(k).num_unique=length(un);
+  data(k).min=un(1);
+  data(k).max=un(end);
+  if false,
+    % histogram of the values in the original ucm2s
+    figure; hist(u(inds))
+  end
+end
+if true
+  % data statistics
+  mean([data.num_nonzeros]) % 815.8 avg number of nonzeros per ucm2 (out of 643*963=619209 pixels)
+  mean([data.num_unique]) % 15.5 avg number of unique values in a ucm2
+  total.un=unique(vertcat(data.nonzeros));
+  length(total.un) % 337
+  total.un(1)   % 0.00061
+  total.un(end) % 0.00245
+end
+
+% output
+outdir.r = [indir.r '_rescaled'];
+[~,~,msgid] = mkdir(path_to_dir,outdir.r);
+% if strcmp(msgid,'MATLAB:MKDIR:DirectoryExists')
+%   warning('Directory already exists.');
+%   return;
+% end
+outdir.a=fullfile(path_to_dir,outdir.r);
