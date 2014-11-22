85a184b2ea1d88fcc94c025b7e3a49c7e4a37a82
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2014-11-22 16:43:15 +0100

    Made the script a function. Handling output (rescaling and saving the new ucm2).

--- auxiliary/rescale_ucm2.m
+++ auxiliary/rescale_ucm2.m
@@ -1,12 +1,12 @@
 % Zornitsa Kostadinova
 % Nov 2014
 % 8.3.0.532 (R2014a)
-% function rescale_ucm2(indir_experiment_name)
+function rescale_ucm2(indir_experiment_name)
+% indir_experiment_name='ucm_bdry_sz_1_ul_1x1'
 % input
-indir_experiment_name='ucm_bdry_sz_1_ul_1x1';
 path_to_dir='/BS/kostadinova/work/video_segm_evaluation/BSDS500/test';
-indir.r = ['Ucm2_' indir_experiment_name]; % relative directory name
-indir.a = fullfile(path_to_dir,indir.r); % absolute directory name
+indir.r=['Ucm2_' indir_experiment_name]; % relative directory name
+indir.a=fullfile(path_to_dir,indir.r); % absolute directory name
 ucm2s=dir(fullfile(indir.a,'*mat'));
 ucm2sz=[643 963];
 ucm2n=length(ucm2s);
@@ -32,8 +32,9 @@ if true
   % data statistics
   mean([data.num_nonzeros]) % 815.8 avg number of nonzeros per ucm2 (out of 643*963=619209 pixels)
   mean([data.num_unique]) % 15.5 avg number of unique values in a ucm2
-  total.un=unique(vertcat(data.nonzeros));
-  length(total.un) % 337
+  total.nonzeros=vertcat(data.nonzeros);
+  total.un=unique(total.nonzeros);
+  total.num_unique=length(total.un) % 337
   total.un(1)   % 0.00061
   total.un(end) % 0.00245
 end
@@ -41,8 +42,21 @@ end
 % output
 outdir.r = [indir.r '_rescaled'];
 [~,~,msgid] = mkdir(path_to_dir,outdir.r);
-% if strcmp(msgid,'MATLAB:MKDIR:DirectoryExists')
-%   warning('Directory already exists.');
-%   return;
-% end
+if strcmp(msgid,'MATLAB:MKDIR:DirectoryExists')
+  warning('Directory already exists. Exiting');
+  return;
+end
 outdir.a=fullfile(path_to_dir,outdir.r);
+
+scale=1 ./ total.un(end);
+% the following rescaling schema would result to the dataset-maximum value
+% being 1 = total.un(end) * scale
+% the dataset-minimum = total.un(1) * scale;
+% for indir_experiment_name='ucm_bdry_sz_1_ul_1x1' the dataset-minimum will be 0.2504
+for k=1:ucm2n
+  % % if you want to exted the data struct with the rescaled result:
+  % data(k).ucm2_rescaled=data(k).ucm2 .* scale;
+  f=matfile(fullfile(outdir.a,ucm2s(k).name),'Writable',true);
+  f.ucm2=data(k).ucm2 .* scale;
+end
+end
