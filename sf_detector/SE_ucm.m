da832f1d72da190bf31674bb0f1424ba38715996
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2014-11-27 16:49:49 +0100

    BUGFIX about the default value of a missed parameter.

--- sf_detector/SE_ucm.m
+++ sf_detector/SE_ucm.m
@@ -2,7 +2,7 @@
 % Nov 2014
 % 8.3.0.532 (R2014a)
 function ucm2 = SE_ucm(I,model,fmt)
-if nargin<2, fmt='imageSize'; end % to comply with default in contours2ucm
+if nargin<3, fmt='imageSize'; end % to comply with default in contours2ucm
 E=edgesDetect(I,model);
 ucm2=contours2ucm(E,fmt);
 end
