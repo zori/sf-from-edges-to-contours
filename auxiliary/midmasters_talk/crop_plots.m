cbda1e7e85d459692c634ed16a0744270e772227
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2014-12-14 12:40:15 +0100

    Helper function to crop the plots that I save from matlab so that they nicely fit in a presentation slide.

--- auxiliary/midmasters_talk/crop_plots.m
+++ auxiliary/midmasters_talk/crop_plots.m
@@ -2,12 +2,12 @@
 % Nov 2014
 % 8.3.0.532 (R2014a)
 function crop_plots()
-orig_dir_a='/home/kostadinova/Dropbox/courses 2014-2015 WiSe/mid-masters presentation/3/results-plots/orig/';
+orig_dir_a='/home/kostadinova/downloads/mid-masters presentation/3/imgs/results-plots/orig/';
 imgs=dir(fullfile(orig_dir_a,'*.png'));
 for k=1:length(imgs)
   plot_name=imgs(k).name;
   orig=imread(fullfile(orig_dir_a,plot_name));
   cropped=orig(35:858,87:1093,:); % nice cropping for plots saved with saveas(gcf,file_name,'png'))
-  imwrite(cropped,fullfile('/home/kostadinova/Dropbox/courses 2014-2015 WiSe/mid-masters presentation/3/results-plots/cropped/',plot_name));
+  imwrite(cropped,fullfile('/home/kostadinova/downloads/mid-masters presentation/3/imgs/results-plots/cropped/',plot_name));
 end
 end
