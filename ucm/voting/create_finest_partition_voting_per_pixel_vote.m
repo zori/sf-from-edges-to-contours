184a27f724275640207d8e202c17a8e1afbe90c1
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2015-02-01 16:17:02 +0100

    Minor experiment to assert that removing 'globalisation' by voting on a single pixels (as opposed to a watershed arc or region boundary) significantly reduces performance.

--- ucm/voting/create_finest_partition_voting_per_pixel_vote.m
+++ ucm/voting/create_finest_partition_voting_per_pixel_vote.m
@@ -1,7 +1,7 @@
 % Zornitsa Kostadinova
 % Oct 2014
 % 8.3.0.532 (R2014a)
-function [sf_wt,votes,vote_fcn,c] = create_finest_partition_voting(pb,vote_fcn,DBG)
+function [sf_wt,votes,vote_fcn,c] = create_finest_partition_voting_per_pixel_vote(pb,vote_fcn,DBG)
 % the extra output args - votes,vote_fcn,c - are only for hard_negatives_mining
 ws=watershed(pb);
 sz=size(pb);
@@ -47,23 +47,52 @@ end % for e - edge index
 
 % apply weights to ucm
 sf_wt=zeros(sz);
+vo2=zeros(sz);
 for e=1:nEdges
   if c.is_completion(e), continue; end
   W=c.edge_weights(e,1)/c.edge_weights(e,2); % avg weight on edge e
   for p=1:numel(c.edge_x_coords{e})
     y=c.edge_x_coords{e}(p); x=c.edge_y_coords{e}(p);
     sf_wt(y,x)=W;
+    assert(~isempty(votes{y,x}));
+    vo2(y,x)=mean(votes{y,x});
   end
   v1=c.vertices(c.edges(e,1),:);
   v2=c.vertices(c.edges(e,2),:);
   sf_wt(v1(1),v1(2))=max(W,sf_wt(v1(1),v1(2)));
   sf_wt(v2(1),v2(2))=max(W,sf_wt(v2(1),v2(2)));
+  if numel(c.edge_x_coords{e})~=0
+    y1=c.edge_x_coords{e}(1); x1=c.edge_y_coords{e}(1);
+    y2=c.edge_x_coords{e}(numel(c.edge_x_coords{e})); x2=c.edge_y_coords{e}(numel(c.edge_x_coords{e}));
+    vo2(v1(1),v1(2))=vo2(y1,x1); % at the vertices, copy the neighboring value from the corresponding edge
+    vo2(v2(1),v2(2))=vo2(y2,x2); %
+  else
+    % NOTE: there are "edges" with no internal nodes, only two end vertices;
+    % how to weight those end vertices?
+    % 1) set to 0,
+    % 2) copy value from corresponding pb(v1(1),v1(2)) and pb(v2(1),v2(2))?
+    vo2(v1(1),v1(2))=pb(v1(1),v1(2)); % 0;
+    vo2(v2(1),v2(2))=pb(v2(1),v2(2)); % 0;
+  end
 end % for e - edge index
 
 % imwrite(uint8((~c.is_e)*255),'/home/kostadinova/downloads/tikis-watershed-arcs-locations.png')
 % imwrite(uint8((1-sf_wt)*255),'/home/kostadinova/downloads/tiki-finest-partition.png')
-% ws_wt=create_finest_partition_non_oriented(pb);
+ws_wt=create_finest_partition_non_oriented(pb);
+% vo=zeros(sz);
+% for y=1:sz(1)
+%   for x=1:sz(2)
+%     v=votes{y,x};
+%     if ~isempty(v)
+%       vo(y,x)=mean(v);
+%     end
+%   end
+% end
+% initFig; im(vo);
+% initFig; im(vo2); % vo and vo2 differ only on the vertices locations (vo2 has values)
+% initFig; im(ws_wt);
 % sf_wt=sf_wt.* ws_wt; % VPR .* pb
 % rgb_loc=cat(3,~~c.is_e,zeros(size(sf_wt)),~~sf_wt);
 % initFig; im(rgb_loc); % reg - watershed arcs that received no votes; blue - vertices of watershed arcs; magenta - watershed arcs that did receive some != 0 votes
+% sf_wt=vo2;
 end
