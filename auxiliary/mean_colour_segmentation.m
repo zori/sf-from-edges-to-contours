c2f2fe2d87c954de645130f08fd9e8e515075c0e
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2015-02-01 13:45:38 +0100

    Renamed to mean_colour_segmentation, as the function could work with any image and its segmentation labels as input.

--- auxiliary/mean_colour_segmentation.m
+++ auxiliary/mean_colour_segmentation.m
@@ -1,7 +1,7 @@
 % Zornitsa Kostadinova
 % Feb 2015
 % 8.3.0.532 (R2014a)
-function Ias = automatic_segmentation(I,labels)
+function mean_colour_segmentation = mean_colour_segmentation(I,labels)
 % given an input image and its segmentation labeling, constructs an image in
 % which each segment is coloured in its mean colour
 assert(size(I,1)==size(labels,1));
@@ -9,14 +9,14 @@ assert(size(I,2)==size(labels,2));
 assert(size(I,3)==3);
 
 num_labels=length(unique(labels));
-Ias=I;
+mean_colour_segmentation=I;
 for l=1:num_labels
   m=labels==l;
   for c=1:3 % each channel - r,g,b
     ic=I(:,:,c);
     mean_colour=mean(ic(m)); % in [0;255]
-    Ias(:,:,c)=uint8(~m).*Ias(:,:,c) + uint8(m)*mean_colour;
+    mean_colour_segmentation(:,:,c)=uint8(~m).*mean_colour_segmentation(:,:,c) + uint8(m)*mean_colour;
   end
 end
-% initFig; im(Ias);
+% initFig; im(mean_colour_segmentation);
 end
