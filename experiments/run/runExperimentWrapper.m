7d861eb0bb84ffcfa697d355704fd71d3e6c8208
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2015-02-10 08:52:28 +0100

    Include pre-setup for the 3 types of 'voting scope' tried - default (nothing) - 1) subdivided watershed arcs 2) mixed - vote on region boundaries, use watershed arcs for fitting (say, line, conic, or contour) 3) region boundary use for both the non-subdivided watershed locations.

--- experiments/run/runExperimentWrapper.m
+++ experiments/run/runExperimentWrapper.m
@@ -3,6 +3,7 @@
 % 8.3.0.532 (R2014a)
 
 % runs a voting experiment and its oracle one after the other
-experiment_name='mixed_voting_scope_foo_rescaled'; % 'region_bdry_line_lls_VPR_normalised_ws';
+% 'region_bdry_scope_foo_rescaled' or 'foo_rescaled'
+experiment_name='mixed_voting_scope_foo_rescaled';
 runExperiment(experiment_name,'voteUcm');
 runExperiment(['oracle_' experiment_name],'oracle');
