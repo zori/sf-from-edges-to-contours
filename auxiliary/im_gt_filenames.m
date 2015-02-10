a8c20534d95042620e8877b56b4ae020f73dcf6d
Zornitsa Kostadinova <kostadinova@mpi-inf.mpg.de>
2015-02-10 08:29:31 +0100

    Added two example images from the test dataset - one with 'hard' edges (overall low UCM values) and one with 'easy' edges (overall the nonzero UCM values are in [0,1]).

--- auxiliary/im_gt_filenames.m
+++ auxiliary/im_gt_filenames.m
@@ -31,4 +31,12 @@ names.starfish.gt='/BS/kostadinova/work/BSR/BSDS500/data/groundTruth/train/12003
 % elephants
 names.elephants.im='/BS/kostadinova/work/BSR/BSDS500/data/images/val/296059.jpg';
 names.elephants.gt='/BS/kostadinova/work/BSR/BSDS500/data/groundTruth/val/296059.mat';
+
+% a head of a statue among greenery - example of (only discovered) very weak edges; "difficult" image
+names.head.im='/BS/kostadinova/work/video_segm_evaluation/BSDS500/test/Images/101084.jpg';
+names.head.gt='/BS/kostadinova/work/video_segm_evaluation/BSDS500/test/Groundtruth/101084.mat';
+
+% polar bear - example of normal, strong edges; "easy" image
+names.polar_bear.im='/BS/kostadinova/work/video_segm_evaluation/BSDS500/test/Images/100007.jpg';
+names.polar_bear.gt='/BS/kostadinova/work/video_segm_evaluation/BSDS500/test/Groundtruth/100007.mat';
 end
